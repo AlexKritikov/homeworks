c5e259109f7ff7ffc633925bd3b2e5309d443cfb
alexkritikov <kritikovalex@gmail.com>
2023-11-04 00:53:42 +0300

    Add HW_18

--- HW_18/18_@property,deskriptors.py
+++ HW_18/18_@property,deskriptors.py
@@ -1,5 +1,5 @@
 if __name__ == '__main__':
-    # """Task 1. Create a class method named `validate`, which should be called from the `__init__` method to validate
+    # Task 1. Create a class method named `validate`, which should be called from the `__init__` method to validate
     # parameter email, passed to the constructor. The logic inside the `validate` method could be to check if the passed
     # email parameter is a valid email string.
 
