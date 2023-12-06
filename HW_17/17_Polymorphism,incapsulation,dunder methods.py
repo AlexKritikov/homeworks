70d0473160e96f03ed31e0037c7a4a10c9aeb698
alexkritikov <kritikovalex@gmail.com>
2023-12-06 18:27:21 +0300

    Add HW_17

--- HW_17/17_Polymorphism,incapsulation,dunder methods.py
+++ HW_17/17_Polymorphism,incapsulation,dunder methods.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
 # Create a base class named Animal with a method called talk and then create two subclasses: Dog and Cat, and make their
 # own implementation of the method talk be different. For instance, Dog’s can be to print ‘woof woof’, while Cat’s can be
 # to print ‘meow’.Also, create a simple generic function, which takes as input instance of a Cat or Dog classes and
-# performs talk method on input parameter.
+# performs talk method on input parameter
     class Animal:
         def __init__(self, name):
             self.name = name
